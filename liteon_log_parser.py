93aaa288ab4767add01e41b7837b6165a1f2b087
James Ensminger <james.ensminger@powerflex.com>
2025-02-05 16:42:22 -0800

    refined log parser abilities, dependent on user inputs

--- liteon_log_parser.py
+++ liteon_log_parser.py
@@ -1,27 +1,58 @@
 import csv
 import re
+from datetime import datetime, timedelta, timezone
 
-def parse_file(path, csv_file, evse_model):
-    evse = evse_model
+def parse_file(path, csv_file, evse_model, test_time):
+    # Convert PST test times to UTC (PST is UTC-8)
+    utc_test_time = []
+    for time in test_time:
+        if time == test_time[2]:
+            if "00:" in utc_test_time[0]:
+                pst_date = datetime.strptime(time, "%m:%d")
+                pst_date = pst_date.replace(year=datetime.now().year)
+                utc_date = pst_date + timedelta(days=1)
+                utc_test_time.append(utc_date.strftime("%a %b %d"))
+                # print(utc_test_time[2])
+            else:
+                pst_date = datetime.strptime(time, "%m:%d")
+                pst_date = pst_date.replace(year=datetime.now().year)
+                utc_date = pst_date.astimezone(timezone.utc)
+                utc_test_time.append(utc_date.strftime("%a %b %d"))
+                # print(utc_test_time[2])
+        else:
+            time_format = "%H:%M"
+            pst_time = datetime.strptime(time, time_format)
+            time_difference = timedelta(hours=8)
+            utc_time = pst_time + time_difference
+            utc_test_time.append(utc_time.strftime(time_format))
+            # print(utc_test_time)
+        print(utc_test_time)
+
     with open(f"{path}\\{csv_file}", 'r') as file:
         reader = csv.reader(file)
 
         hop_count = 0
+        in_range = False
         hz_change = False
         wap_connected = True
-        match = ""
         pattern = [r"WIFI CHANGE FREQUENCY from [0-9]*\.[0-9]+ GHz to [0-9]*\.[0-9]+ GHz",
-                   r"WIFI CONNECTED to ([A-Za-z0-9]+(:[A-Za-z0-9]+)+)  [0-9]*\.[0-9]+ GHz"]
+                   r"WIFI CONNECTED to ([A-Za-z0-9]+(:[A-Za-z0-9]+)+)  [0-9]*\.[0-9]+ GHz",
+                   rf"{utc_test_time[0]}:[0-9]+",    # start time of test
+                   rf"{utc_test_time[1]}:[0-9]+",    # end time of test
+                   utc_test_time[2]]                 # test date
 
         for row in reader:
-            match = re.search(pattern[0], row[0])
-            if wap_connected and match:
-                wap_connected = False
-                hz_change = True
-            match = re.search(pattern[1], row[0])
-            if hz_change and match:
-                wap_connected = True
-                hz_change = False
-                hop_count += 1
+            # if (re.search(pattern[2], row[0]) and re.search(pattern[4], row[0])) or in_range:
+            if (re.search(pattern[4], row[0]) and re.search(pattern[2], row[0])) or in_range:
+                in_range = True
+                if re.search(pattern[3], row[0]):
+                    break
+                elif wap_connected and re.search(pattern[0], row[0]):
+                    wap_connected = False
+                    hz_change = True
+                elif hz_change and re.search(pattern[1], row[0]):
+                    wap_connected = True
+                    hz_change = False
+                    hop_count += 1
 
-    print(f"# of WAP hops ({evse}): {hop_count}")
+    print(f"# of WAP hops ({evse_model}): {hop_count}")
--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import os
 import liteon_log_parser
 
+# Grab user input of test time frame in 24hr format
+def get_input(user_input_list):
+    user_input_list.append(input("Enter start time of test in 24hr format (hh:mm): "))
+    user_input_list.append(input("Enter end time of test in 24hr format (hh:mm): "))
+    user_input_list.append(input("Enter date of test (mm:dd): "))
+    return user_input_list
+
+# Find charger's log and parse it
 path = "C:\\Users\\scripts\\evse-wifi-mesh-hopping\\evse-wifi-mesh-hopping\\logs"
+user_input = []
 for filename in os.listdir(path):
     if filename.__contains__("SC48") and filename.endswith(".csv"):
-        liteon_log_parser.parse_file(path, filename, "SC48")
+        print(f"{filename} is loading...")
+        get_input(user_input)
+        liteon_log_parser.parse_file(path, filename, "SC48", user_input)
     elif filename.__contains__("SC48_POS") and filename.endswith(".csv"):
-        liteon_log_parser.parse_file(path, filename, "SC48_POS")
+        print(f"{filename} is loading...")
+        get_input(user_input)
+        liteon_log_parser.parse_file(path, filename, "SC48_POS", user_input)
     elif filename.__contains__("IC48") and filename.endswith(".csv"):
-        liteon_log_parser.parse_file(path, filename, "IC48")
+        print(f"{filename} is loading...")
+        get_input(user_input)
+        liteon_log_parser.parse_file(path, filename, "IC48", user_input)
     elif filename.__contains__("IC80") and filename.endswith(".csv"):
-        liteon_log_parser.parse_file(path, filename, "IC80")
+        print(f"{filename} is loading...")
+        get_input(user_input)
+        liteon_log_parser.parse_file(path, filename, "IC80", user_input)
     else:
         print("No CSV formatted log files found.")
         break
+    user_input.clear()
